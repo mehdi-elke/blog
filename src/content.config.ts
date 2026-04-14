16ddfe7306fa30f7194cade4ef20bb69c315ce31
mehdi-elke <m.elketroussi@gmail.com>
2026-04-14 16:44:28 +0200

    fix build

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,11 +1,13 @@
 import { defineCollection, z } from 'astro:content';
+import { glob } from 'astro/loaders';
 
 const blog = defineCollection({
-	type: 'content',
+	// Use the 'glob' loader to find files in 'src/content/blog'
+	loader: glob({ pattern: '**/[^_]*.{md,mdx}', base: "./src/content/blog" }),
 	schema: z.object({
 		title: z.string(),
 		description: z.string(),
-		pubDate: z.date(),
+		pubDate: z.coerce.date(),
 		tags: z.array(z.string()).optional(),
 	}),
 });
