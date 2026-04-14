bf422d245e27048a84484e1d3b3a5d937a8d9b99
mehdi-elke <m.elketroussi@gmail.com>
2026-04-14 16:25:31 +0200

    Initial commit: FAILSAFE blog

---
+++ astro.config.mjs
@@ -0,0 +1,14 @@
+import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://mehdi-elke.github.io',
+  base: '/blog',
+  vite: {
+    plugins: [tailwindcss()]
+  },
+  integrations: [mdx(), sitemap()]
+});
